ef28310308183c2284315e7dc63f6cd0753b4555
Dylan <dylan.imail@gmail.com>
2023-06-13 17:15:31 -0400

    quick push

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func (t Task) Description() string {
 /* MAIN MODEL */
 
 type Model struct {
-    list list.Model
+    lists []list.Model
     err error
 }
 
@@ -49,10 +49,11 @@ func New() *Model {
 }
 
 // TODO: call this on tea.WindowSizeMsg
-func (m *Model) initList(width, height int) {
-    m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-    m.list.Title = "To Do"
-    m.list.SetItems([]list.Item{
+func (m *Model) initLists(width, height int) {
+    defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+    m.lists = []list.Model{defaultList, defaultList, defaultList}
+    m.lists.Title = "To Do"
+    m.lists.SetItems([]list.Item{
         Task{status: todo, title: "buy milk", description: "strawberry milk"},
         Task{status: todo, title: "seat sushi", description: "negitoro roll, miso soup"},
         Task{status: todo, title: "fild laundry", description: "or wear wrinkly t-shirts"},
@@ -66,15 +67,15 @@ func (m Model) Init() tea.Cmd {
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
         case tea.WindowSizeMsg:
-            m.initList(msg.Width, msg.Height)
+            m.initLists(msg.Width, msg.Height)
     }
     var cmd tea.Cmd
-    m.list, cmd = m.list.Update(msg)
+    m.lists, cmd = m.lists.Update(msg)
     return m, cmd
 }
 
 func (m Model) View() string {
-    return m.list.View()
+    return m.lists.View()
 }
 
 func main() {
